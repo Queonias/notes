0818bbd2ed312f13d35645777d73f0e3489f7c68
queonias <queones@gmail.com>
2023-06-28 17:09:30 -0300

    feat: Handlebars view engine configuration

--- src/app.js
+++ src/app.js
@@ -1,23 +1,40 @@
 const express = require('express');
 const path = require('path');
 require('dotenv').config();
+const exphbs = require('express-handlebars');
 
 // Initializations
 const app = express();
 
 // Settings
-app.set('port', process.env.PORT || 3001);
+// Define a porta do aplicativo usando a variável de ambiente PORT
+app.set("port", process.env.PORT);
+
+// Configura o diretório de visualizações como sendo o diretório 'views' dentro do diretório atual
 app.set('views', path.join(__dirname, 'views'));
 
+// Configuração do mecanismo de visualização Handlebars
+const hbs = exphbs.create({
+    defaultLayout: "main",
+    layoutsDir: path.join(app.get("views"), "layouts"),
+    partialsDir: path.join(app.get("views"), "partials"),
+    extname: ".hbs",
+  });
+  app.engine(".hbs", hbs.engine);
+  app.set("view engine", ".hbs");
+
 // Middlewares
+// Adiciona o middleware para analisar os dados do corpo da solicitação com o formato de codificação x-www-form-urlencoded
 app.use(express.urlencoded({ extended: false }));
 
 // Routes
+// Rota raiz que renderiza a visualização "index"
 app.get('/', (_req, res) => {
-    res.send('hello word')
+    res.render('index')
 });
 
 // Static files
+// Define o diretório 'public' como o diretório de arquivos estáticos
 app.use(express.static(path.join(__dirname, 'public')));
 
 
