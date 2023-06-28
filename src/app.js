41f816abe63c54c725a60a5f22e7e2426b90b945
queonias <queones@gmail.com>
2023-06-28 09:58:34 -0300

    feat: app.js && server.js

---
+++ src/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+
+// Initializations
+const app = express();
+
+// Settings
+app.set('port', process.env.PORT || 3001);
+app.set('views', path.join(__dirname, 'views'));
+
+// Middlewares
+app.use(express.urlencoded({ extended: false }));
+
+// Routes
+app.get('/', (_req, res) => {
+    res.send('hello word')
+});
+
+// Static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+module.exports = app;
