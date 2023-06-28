50737645488730dc21f2916b93ca7fd654186d62
queonias <queones@gmail.com>
2023-06-28 15:37:13 -0300

    feat: user table schema created

---
+++ models/User.js
@@ -0,0 +1,35 @@
+const { Schema, model} = require('mongoose');
+const bcryptjs = require('bcryptjs');
+
+// Definindo o esquema do usuário
+const UserSchema = new Schema({
+    name: { 
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+}, {
+    timestamps: true
+});
+
+// Método para criptografar a senha do usuário
+UserSchema.methods.encryptPassword = async password => {
+   const salt = await bcryptjs.genSalt(10);
+   return await bcryptjs.hash(password, salt)
+};
+
+// Método para comparar a senha fornecida com a senha armazenada do usuário
+UserSchema.methods.matchPassword = async function (password) {
+   return await bcryptjs.compare(password, this.password)
+}
+
+
+// Exportando o modelo do usuário com o esquema definido
+module.exports = model('User', UserSchema);
