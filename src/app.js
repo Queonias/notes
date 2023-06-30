89f9ecc66ee28fd4fab13c5a6f0d133f3aef04db
queonias <queones@gmail.com>
2023-06-30 11:00:18 -0300

    feat: Messages with connect-flash

--- src/app.js
+++ src/app.js
@@ -4,17 +4,19 @@ const path = require('path');
 require('dotenv').config();
 const exphbs = require('express-handlebars');
 const methodOverride = require('method-override');
+const flash = require('connect-flash');
+const session = require('express-session');
+
 
 // Initializations
 const app = express();
 
+
 // Settings
 // Define a porta do aplicativo usando a variável de ambiente PORT
 app.set("port", process.env.PORT);
-
 // Configura o diretório de visualizações como sendo o diretório 'views' dentro do diretório atual
 app.set('views', path.join(__dirname, 'views'));
-
 // Configuração do mecanismo de visualização Handlebars
 const hbs = exphbs.create({
     defaultLayout: "main",
@@ -25,12 +27,24 @@ const hbs = exphbs.create({
   app.engine(".hbs", hbs.engine);
   app.set("view engine", ".hbs");
 
+
 // Middlewares
 app.use(morgan('dev'));
-app.use(methodOverride('_method'))
-
+app.use(methodOverride('_method'));
+app.use(session({
+  secret: 'secret',
+  resave: true,
+  saveUninitialized: true,
+}));
+app.use(flash());
 // Adiciona o middleware para analisar os dados do corpo da solicitação com o formato de codificação x-www-form-urlencoded
 app.use(express.urlencoded({ extended: false }));
+
+// Global Variables
+app.use((req, res, next) => {
+  res.locals.success_msg = req.flash('success_msg');
+  next();
+});
 
 // Routes
 // Rota raiz que renderiza a visualização "index"
--- src/controllers/notes.contrller.js
+++ src/controllers/notes.contrller.js
@@ -1,4 +1,5 @@
 const Notes = require('../models/Note');
+// const flash = require('connect-flash');
 const notesCtrl = {}
 
 notesCtrl.renderNoteForm = (_req, res) => {
@@ -7,8 +8,9 @@ notesCtrl.renderNoteForm = (_req, res) => {
 
 notesCtrl.createNewNote = async (req, res) => {
     const { title, description} = req.body;
-    const newNote = new Notes({ title, description })
+    const newNote = new Notes({ title, description });
     await newNote.save();
+    req.flash("success_msg", "Note Added Successfully");
     res.redirect('/notes');    
 }
 
@@ -25,11 +27,13 @@ notesCtrl.renderEditForm = async (req, res) => {
 notesCtrl.updateNote = async (req, res) => {
     const { title, description } = req.body;
     await Notes.findByIdAndUpdate(req.params.id, { title, description });
+    req.flash("success_msg", "Note Updated Successfully");
     res.redirect('/notes');   
 }
 
 notesCtrl.deleteNote = async (req, res) => {
     await Notes.findByIdAndDelete(req.params.id);
+    req.flash("success_msg", "Note Deleted Successfully");
     res.redirect('/notes'); 
 }
 
